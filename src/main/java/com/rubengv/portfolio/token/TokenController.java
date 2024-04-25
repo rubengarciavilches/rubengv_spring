a751a583151703672d22681fbf8c163d5cb0cd48
Rubén García <37160008+rubengarciavilches@users.noreply.github.com>
2024-04-25 17:19:58 +0200

    Removing Cookies
    Removing cookies given the problems that they cause in the communication between backend and frontend.

--- src/main/java/com/rubengv/portfolio/token/TokenController.java
+++ src/main/java/com/rubengv/portfolio/token/TokenController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(path = "api/v1/auth")
 //@CrossOrigin(origins = {"http://localhost:3000", "https://www.rubengv.com"}, allowCredentials = "true", exposedHeaders = "Authorization")
-public class TokenController {
+public class TokenController { 
     private final TokenService tokenService;
 
     @Autowired
